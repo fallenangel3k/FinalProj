39d3d8e288dad91e15f53d3329ad8a749bd94050
Matthew McCormick <mr2mccor@gmail.com>
2016-03-28 16:14:15 -0400

    Got a static class method working.

--- src/cljwallet/core.clj
+++ src/cljwallet/core.clj
@@ -1,11 +1,14 @@
 (ns cljwallet.core
   (:require [clojure.data.json :as json]
             [org.httpkit.client :as http])
-  (:import  (org.bitcoinj.core BlockChain Coin)))
+  (:import  (org.bitcoinj.params MainNetParams)))
+
+;(defn -main [& args]
+;  (def addressInfo @(http/get "https://api.blockcypher.com/v1/btc/main/addrs/1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2"))
+;  (let [{a :body} addressInfo]
+;    (print a)))
 
 (defn -main [& args]
-  (def addressInfo @(http/get "https://api.blockcypher.com/v1/btc/main/addrs/1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2"))
-  (let [{a :body} addressInfo]
-    (print a)))
-
-
+  (def params (. MainNetParams get))
+  (println params)
+  (def filePrefix "forwarding-service"))
